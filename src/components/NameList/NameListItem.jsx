eccfac33a7cb188543b6a7daca9771fbc2831544
udara <uvlakshan@gmail.com>
2020-10-09 23:55:38 +0530

    Added alt with props.name

--- src/components/NameList/NameListItem.jsx
+++ src/components/NameList/NameListItem.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 function NameListItem(props){
     return (
            <li>
-             <p><img src={props.avatar} /> {props.name}</p>
+             <p><img src={props.avatar} alt= {props.name} /> {props.name}</p>
              <p>City:{props.city} </p>{}
              <p>Email:{props.email}</p>
              <p>Birthday:{moment(props.birthday).format('DD-MM-YYYY')}</p>
