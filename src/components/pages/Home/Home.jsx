b56507f0b93f503416beeeda729231ee68777217
udara <uvlakshan@gmail.com>
2020-10-13 23:12:25 +0530

    Did some changes in the body

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -5,7 +5,9 @@ function Home(){
         <React.Fragment>
             <div className="container mt-4">
                 <h3>Welcome</h3>
-                <p>sddddddddkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</p>
+                <p>React has been designed from the start for gradual adoption, and you can use as little or as much React as you need. Whether you want to get a taste of React, add some interactivity to a simple HTML page, or start a complex React-powered app, the links in this section will help you get started.
+
+</p>
             </div>
         </React.Fragment>
     );
