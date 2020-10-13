6f1243c1568737afc638d57dcd22441b06bb6271
udara <uvlakshan@gmail.com>
2020-10-13 23:19:12 +0530

    Chages some lines

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -5,7 +5,7 @@ function About(){
         <React.Fragment>
             <div className="container mt-4">
                 <h3>About</h3>
-                <p>skkkkkwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk</p>
+                <p>React (also known as React.js or ReactJS) is an open-source, front end, JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.</p>
             </div>
         </React.Fragment>
     );
